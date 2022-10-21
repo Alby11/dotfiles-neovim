ccbe7370b1cc7aa8c9673ecddf5f2b8037063abb
alby11 <17138674+Alby11@users.noreply.github.com>
2022-10-21 17:06:36 +0200

    refactor in lua language

--- lua/plugins/_sqlite.lua
+++ lua/plugins/_sqlite.lua
@@ -1,6 +1,4 @@
 if is_win then
-  cmd([[
-        let g:sqlite_clib_path=config_dir . '/databases/sqlite3.dll'
-    ]])
+    vim.g.sqlite_clib_path = string.format("%s/databases/sqlite3.dll", Config_dir)
 end
 local sqlite = require("sqlite")
