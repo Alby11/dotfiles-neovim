121ea681cb3c1d5fd2ee8a2fb785baacac8f44a1
alby11 <17138674+alby11@users.noreply.github.com>
2022-10-28 16:10:32 +0200

    added mason-null-ls

--- lua/plugins/_lsp-zero.lua
+++ lua/plugins/_lsp-zero.lua
@@ -5,20 +5,6 @@ if not CheckPlugin(plugin_name) then
 end
 
 Lsp = require(plugin_name)
-Lsp.set_preferences({
-  suggest_lsp_servers = true,
-  setup_servers_on_start = true,
-  set_lsp_keymaps = true,
-  configure_diagnostics = true,
-  cmp_capabilities = true,
-  manage_nvim_cmp = true,
-  call_servers = "local",
-  sign_icons = {
-    error = "✘",
-    warn = "▲",
-    hint = "⚑",
-    info = "",
-  },
-})
+Lsp.preset("recommended")
 Lsp.nvim_workspace()
 Lsp.setup()
