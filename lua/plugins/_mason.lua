49dd92bd8cfba3a98af75fa73dd20f5efaf07999
alby11 <alby11@duck.com>
2022-12-01 17:36:56 +0100

    removed powershell_es

--- lua/plugins/_mason.lua
+++ lua/plugins/_mason.lua
@@ -1,67 +1,65 @@
 -- import mason plugin safely
 local mason_status, mason = pcall(require, "mason")
 if not mason_status then
-  return
+	return
 end
 
 -- import mason-lspconfig plugin safely
 local mason_lspconfig_status, mason_lspconfig = pcall(require, "mason-lspconfig")
 if not mason_lspconfig_status then
-  return
+	return
 end
 
 -- import mason-null-ls plugin safely
 local mason_null_ls_status, mason_null_ls = pcall(require, "mason-null-ls")
 if not mason_null_ls_status then
-  return
+	return
 end
 
 -- enable mason
 mason.setup()
 
 mason_lspconfig.setup({
-  -- list of servers for mason to install
-  ensure_installed = {
-    "ansiblels",
-    "bashls",
-    "cssls",
-    "dockerls",
-    "emmet_ls",
-    "gopls",
-    "html",
-    "marksman",
-    "powershell_es",
-    "pyright",
-    "rust_analyzer",
-    "sumneko_lua",
-    "sqlls",
-    "tailwindcss",
-    "tsserver",
-    "vimls",
-    "yamlls"
-  },
-  -- auto-install configured servers (with lspconfig)
-  automatic_installation = true, -- not the same as ensure_installed
+	-- list of servers for mason to install
+	ensure_installed = {
+		"ansiblels",
+		"bashls",
+		"cssls",
+		"dockerls",
+		"emmet_ls",
+		"gopls",
+		"html",
+		"marksman",
+		"pyright",
+		"sumneko_lua",
+		"sqlls",
+		"tailwindcss",
+		"tsserver",
+		"vimls",
+		"yamlls",
+	},
+	-- auto-install configured servers (with lspconfig)
+	automatic_installation = true, -- not the same as ensure_installed
 })
 
 mason_null_ls.setup({
-  -- list of formatters & linters for mason to install
-  ensure_installed = {
-    "actionlint", -- yanl
-    "djlint",
-    "eslint_d", -- ts/js linter
-    "fixjson",
-    "flake8",
-    "gitcommit",
-    "gofumpt",
-    "hadolint",
-    "hadolint",
-    "markdownlint",
-    "prettier", -- ts/js formatter
-    "shfmt",
-    "sqlfluff",
-    "stylua", -- lua formatter
-  },
-  -- auto-install configured formatters & linters (with null-ls)
-  automatic_installation = true,
+	-- list of formatters & linters for mason to install
+	ensure_installed = {
+		"actionlint", -- yanl
+		"djlint",
+		"eslint_d", -- ts/js linter
+		"fixjson",
+		"flake8",
+		"gitcommit",
+		"gofumpt",
+		"hadolint",
+		"hadolint",
+		"markdownlint",
+		"prettier", -- ts/js formatter
+		"shfmt",
+		"sqlfluff",
+		"stylua", -- lua formatter
+	},
+	-- auto-install configured formatters & linters (with null-ls)
+	automatic_installation = true,
 })
