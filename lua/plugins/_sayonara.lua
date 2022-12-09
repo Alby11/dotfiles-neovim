68d7b2470d1fead7d33f4a1a7b9781f302c49009
alby11 <alby11@duck.com>
2022-12-09 09:06:06 +0100

    plugins cleanup and solved messed chars (opt.tile)

--- lua/plugins/_sayonara.lua
+++
@@ -1,6 +0,0 @@
-vim.cmd([[let g:sayonara_confirm_quit = 1]])
-
-local map = vim.api.nvim_set_keymap
-local opts = { noremap = true, silent = true }
-
-map("n", "<Leader>bs", "<Cmd>Sayonara<CR>", opts)
