84386a5677918433d939e3323924065939f2b3ac
alby11 <alby11@duck.com>
2023-09-04 15:34:22 +0200

    remove legacy tag fidget

--- lua/packer-use.lua
+++ lua/packer-use.lua
@@ -38,7 +38,7 @@ return Packer.startup(function(use)
 		config = function()
 			require("fidget").setup()
 		end,
-		tag = "legacy",
+		-- tag = "legacy",
 	})
 	use("elijahmanor/export-to-vscode.nvim")
 
