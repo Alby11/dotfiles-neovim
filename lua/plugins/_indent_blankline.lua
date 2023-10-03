9779a8246751ea8ec23b0a9490db82f2dac9167d
alby11 <alby11@duck.com>
2023-10-03 17:44:38 +0200

    refactor for v3

--- lua/plugins/_indent_blankline.lua
+++ lua/plugins/_indent_blankline.lua
@@ -1,33 +1,24 @@
-vim.opt.termguicolors = true
--- vim.cmd([[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]])
--- vim.cmd([[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]])
--- vim.cmd([[highlight IndentBlanklineIndent3 guifg=#98C379 gui=nocombine]])
--- vim.cmd([[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]])
--- vim.cmd([[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]])
--- vim.cmd([[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]])
+local highlight = {
+	"RainbowRed",
+	"RainbowYellow",
+	"RainbowBlue",
+	"RainbowOrange",
+	"RainbowGreen",
+	"RainbowViolet",
+	"RainbowCyan",
+}
 
-vim.cmd([[highlight IndentBlanklineIndent1 guifg=#ff79c6 gui=nocombine]])
-vim.cmd([[highlight IndentBlanklineIndent2 guifg=#ffb86c gui=nocombine]])
-vim.cmd([[highlight IndentBlanklineIndent3 guifg=#50fa7b gui=nocombine]])
-vim.cmd([[highlight IndentBlanklineIndent4 guifg=#8be9fd gui=nocombine]])
-vim.cmd([[highlight IndentBlanklineIndent5 guifg=#6272a4 gui=nocombine]])
-vim.cmd([[highlight IndentBlanklineIndent6 guifg=#bd93f9 gui=nocombine]])
+local hooks = require("ibl.hooks")
+-- create the highlight groups in the highlight setup hook, so they are reset
+-- every time the colorscheme changes
+hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+	Api.nvim_set_hl(0, "RainbowRed", { fg = "#ff6c6b" }) -- Mocha Red
+	Api.nvim_set_hl(0, "RainbowYellow", { fg = "#ECBE7B" }) -- Mocha Yellow
+	Api.nvim_set_hl(0, "RainbowBlue", { fg = "#51afef" }) -- Mocha Blue
+	Api.nvim_set_hl(0, "RainbowOrange", { fg = "#da8548" }) -- Mocha Orange
+	Api.nvim_set_hl(0, "RainbowGreen", { fg = "#98be65" }) -- Mocha Green
+	Api.nvim_set_hl(0, "RainbowViolet", { fg = "#c678dd" }) -- Mocha Violet
+	Api.nvim_set_hl(0, "RainbowCyan", { fg = "#46D9FF" }) -- Mocha Cyan
+end)
 
-vim.opt.list = true
-vim.opt.listchars:append("space:⋅")
-vim.opt.listchars:append("eol:↴")
-
-require("indent_blankline").setup({
-	show_end_of_line = true,
-	space_char_blankline = " ",
-	show_current_context = true,
-	show_current_context_start = true,
-	char_highlight_list = {
-		"IndentBlanklineIndent1",
-		"IndentBlanklineIndent2",
-		"IndentBlanklineIndent3",
-		"IndentBlanklineIndent4",
-		"IndentBlanklineIndent5",
-		"IndentBlanklineIndent6",
-	},
-})
+require("ibl").setup({ indent = { highlight = highlight } })
