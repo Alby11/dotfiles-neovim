4a754fbe61d7acc3dbfc015bb04dc0565f11848f
alby11 <alby11@duck.com>
2023-12-07 17:09:28 +0100

    custom dash logo

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -14,6 +14,82 @@ return {
   { "ellisonleao/gruvbox.nvim" },
   { "catppuccin/nvim" },
 
+
+  {
+    "nvimdev/dashboard-nvim",
+    event = "VimEnter",
+    opts = function()
+      -- Function to execute a shell command and capture its output
+      -- to get the current user@hostname
+      local function execute_command(command)
+          local handle = io.popen(command)
+          local result = handle:read("*a")
+          handle:close()
+          return result
+      end
+      -- Get the username
+      local username = execute_command("whoami"):gsub("\n", "")
+      -- Get the hostname
+      local hostname = execute_command("hostname"):gsub("\n", "")
+      local boxWidth = 80
+      local padding = (boxWidth - string.len(username) - string.len(hostname) - 11) / 2
+      local logo = string.format([[
+          ╔══════════════════════════════════════════════════════════════════════════════════════════╗
+                           User@Host: ]] .. username .. '@' .. hostname .. [[                         
+          ╚══════════════════════════════════════════════════════════════════════════════════════════╝
+      ]], string.rep(" ", padding), username, hostname, string.rep(" ", padding))
+      logo = string.rep("\n", 8) .. logo .. "\n\n"
+
+      local opts = {
+        theme = "doom",
+        hide = {
+          -- this is taken care of by lualine
+          -- enabling this messes up the actual laststatus setting after loading a file
+          statusline = false,
+        },
+        config = {
+          header = vim.split(logo, "\n"),
+          -- stylua: ignore
+          center = {
+            { action = "Telescope find_files",                                     desc = " Find file",       icon = " ", key = "f" },
+            { action = "ene | startinsert",                                        desc = " New file",        icon = " ", key = "n" },
+            { action = "Telescope oldfiles",                                       desc = " Recent files",    icon = " ", key = "r" },
+            { action = "Telescope live_grep",                                      desc = " Find text",       icon = " ", key = "g" },
+            { action = [[lua require("lazyvim.util").telescope.config_files()()]], desc = " Config",          icon = " ", key = "c" },
+            { action = 'lua require("persistence").load()',                        desc = " Restore Session", icon = " ", key = "s" },
+            { action = "LazyExtras",                                               desc = " Lazy Extras",     icon = " ", key = "x" },
+            { action = "Lazy",                                                     desc = " Lazy",            icon = "󰒲 ", key = "l" },
+            { action = "qa",                                                       desc = " Quit",            icon = " ", key = "q" },
+          },
+          footer = function()
+            local stats = require("lazy").stats()
+            local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+            return { "⚡ Neovim loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms .. "ms" }
+          end,
+        },
+      }
+
+      for _, button in ipairs(opts.config.center) do
+        button.desc = button.desc .. string.rep(" ", 43 - #button.desc)
+        button.key_format = "  %s"
+      end
+
+      -- close Lazy and re-open when the dashboard is ready
+      if vim.o.filetype == "lazy" then
+        vim.cmd.close()
+        vim.api.nvim_create_autocmd("User", {
+          pattern = "DashboardLoaded",
+          callback = function()
+            require("lazy").show()
+          end,
+        })
+      end
+
+      return opts
+    end,
+  },
+
+
   {
     "kiyoon/jupynium.nvim",
     build = "pip3 install --user .",
