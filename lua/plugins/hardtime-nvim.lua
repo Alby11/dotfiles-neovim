3beba7cf335fee3591d601586a210c2ed482bf5c
alby11 <alby11@duck.com>
2024-01-05 22:16:15 +0100

    enable hardtime

--- lua/plugins/hardtime-nvim.lua
+++ lua/plugins/hardtime-nvim.lua
@@ -1,7 +1,7 @@
 local mapkey = require("util.keymapper").mapvimkey
 return {
 	"m4xshen/hardtime.nvim",
-	enabled = false,
+	enabled = true,
 	command = "Hardtime",
 	event = "BufEnter",
 	dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
