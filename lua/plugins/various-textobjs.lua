2aedb01963a2e7fbd718201892c9aa6a18645bc2
alby11 <alby11@duck.com>
2024-02-13 15:23:15 +0100

    add various-textobjs, conf by CristianChiarulli/nvim

---
+++ lua/plugins/various-textobjs.lua
@@ -0,0 +1,14 @@
+local M = {
+  "chrisgrieser/nvim-various-textobjs",
+  enabled = true,
+  lazy = false,
+}
+
+function M.config()
+  require("various-textobjs").setup { useDefaultKeymaps = false }
+
+  vim.keymap.set({ "o", "x" }, "aw", '<cmd>lua require("various-textobjs").subword("outer")<CR>')
+  vim.keymap.set({ "o", "x" }, "iw", '<cmd>lua require("various-textobjs").subword("inner")<CR>')
+end
+
+return M
